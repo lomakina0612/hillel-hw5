bbe1572b28f3e840ae58c5864af2e224025c8ac9
Elena Lomakina <lomakina0612@gmail.com>
2024-08-19 16:47:21 +0200

    add the number of windows and the option of multiple bathrooms

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
 class Premises:
-    def __init__(self, flooring_type):
+    def __init__(self, flooring_type, windows_count: int):
         self.flooring_type = flooring_type
+        self.windows_count = windows_count
 
     def __str__(self):
-        return f"Flooring: {self.flooring_type}"
+        return f"Flooring: {self.flooring_type}, Windows: {self.windows_count}"
 
 class RectangularPremises(Premises):
-    def __init__(self, length, width, flooring_type):
+    def __init__(self, length, width, flooring_type, windows_count: int):
+        super().__init__(flooring_type, windows_count)
         self.length = length
         self.width = width
-        self.flooring_type = flooring_type
         self.area = self.calculate_area()
         self.perimeter = self.calculate_perimeter()
 
@@ -20,20 +22,20 @@ class RectangularPremises(Premises):
         return 2 * (self.length + self.width)
 
     def __str__(self):
-        return f"Area: {self.area} sqm, Perimeter: {self.perimeter} m, Flooring: {self.flooring_type}"
+        return f"Area: {self.area} sqm, Perimeter: {self.perimeter} m, Flooring: {self.flooring_type}, Windows: {self.windows_count}"
 
 class NonRectangularPremises(Premises):
-    def __init__(self, area, perimeter, flooring_type):
-        super().__init__(flooring_type)
+    def __init__(self, area, perimeter, flooring_type, windows_count: int):
+        super().__init__(flooring_type, windows_count)
         self.area = area
         self.perimeter = perimeter
 
     def __str__(self):
-        return f"Area: {self.area} sqm, Perimeter: {self.perimeter} m, Flooring: {self.flooring_type}"
+        return f"Area: {self.area} sqm, Perimeter: {self.perimeter} m, Flooring: {self.flooring_type}, Windows: {self.windows_count}"
 
 class Kitchen(RectangularPremises):
-    def __init__(self, length, width, flooring_type, has_dishwasher, appliance_count):
-        super().__init__(length, width, flooring_type)
+    def __init__(self, length, width, flooring_type, windows_count: int, has_dishwasher, appliance_count: int):
+        super().__init__(length, width, flooring_type, windows_count)
         self.has_dishwasher = has_dishwasher
         self.appliance_count = appliance_count
 
@@ -42,57 +44,53 @@ class Kitchen(RectangularPremises):
                 f"Appliances: {self.appliance_count}")
 
 class Bathroom(RectangularPremises):
-    def __init__(self, length, width, flooring_type, has_bathtub, has_shower):
-        super().__init__(length, width, flooring_type)
+    def __init__(self, length, width, flooring_type, windows_count: int, has_bathtub, has_shower, has_toilet, bathroom_type):
+        super().__init__(length, width, flooring_type, windows_count)
         self.has_bathtub = has_bathtub
         self.has_shower = has_shower
+        self.has_toilet = has_toilet
+        self.bathroom_type = bathroom_type
 
     def __str__(self):
-        return (f"Bathroom - {super().__str__()}, Bathtub: {'Yes' if self.has_bathtub else 'No'}, "
-                f"Shower: {'Yes' if self.has_shower else 'No'}")
+        return (f"{self.bathroom_type} - {super().__str__()}, Bathtub: {'Yes' if self.has_bathtub else 'No'}, "
+                f"Shower: {'Yes' if self.has_shower else 'No'}, Toilet: {'Yes' if self.has_toilet else 'No'}")
 
 class Room(RectangularPremises):
-    def __init__(self, length, width, flooring_type, room_type):
-        super().__init__(length, width, flooring_type)
+    def __init__(self, length, width, flooring_type, windows_count: int, room_type):
+        super().__init__(length, width, flooring_type, windows_count)
         self.room_type = room_type
 
     def __str__(self):
         return f"{self.room_type} - {super().__str__()}"
 
 class NonRectangularRoom(NonRectangularPremises):
-    def __init__(self, area, perimeter, flooring_type, room_type):
-        super().__init__(area, perimeter, flooring_type)
+    def __init__(self, area, perimeter, flooring_type, windows_count: int, room_type):
+        super().__init__(area, perimeter, flooring_type, windows_count)
         self.room_type = room_type
 
     def __str__(self):
-            return f"{self.room_type} - {super().__str__()}"
+        return f"{self.room_type} - {super().__str__()}"
     
 class Flat:
-    def __init__(self, address, rooms:list, kitchen, bathroom):
+    def __init__(self, address, premises):
         self.address = address
-        self.rooms = rooms  # List of Room objects
-        self.kitchen = kitchen
-        self.bathroom = bathroom
+        self.premises = premises
 
     def total_area(self):
-        return sum([room.area for room in self.rooms]) + self.kitchen.area + self.bathroom.area
+        return sum([premise.area for premise in self.premises])
 
     def __str__(self):
-        return (f"Flat at {self.address}\nTotal Area: {self.total_area()} sqm\n\n"
-                f"{self.kitchen}\n{self.bathroom}\n" +
-                "\n".join(str(room) for room in self.rooms))
+        return (f"Flat at {self.address}\nTotal Area: {self.total_area()} sqm\n\n" +
+                "\n".join(str(premise) for premise in self.premises))
 
-
-# Приклад використання
-
-
-
-kitchen = Kitchen(3, 5, "Tile", True, 4)
-bathroom = Bathroom(2, 4, "Marble", True, True)
-living_room = Room(5, 4, "Wood", "Living Room")
-bedroom = Room(5, 3, "Carpet", "Bedroom")
-children_room = NonRectangularRoom(16, 18, "Wood", "Children room")
-terrace = NonRectangularRoom(6, 12, "Tile", "Terrace")
-flat = Flat("22 Kanatna Street, Odesa", [living_room, bedroom, children_room, terrace], kitchen, bathroom)
+# Example
+kitchen = Kitchen(3, 5, "Tile", 1, True, 4)
+master_bathroom  = Bathroom(2, 4, "Marble", 1, True, True, True, "Master Bathroom")
+guest_toilet = Bathroom(1.5, 2, "Tile", 0, False, False, True, "Guest Toilet")
+living_room = Room(5, 4, "Wood", 1, "Living Room")
+bedroom = Room(5, 3, "Carpet", 1, "Bedroom")
+children_room = NonRectangularRoom(16, 18, "Wood", 2, "Children Room")
+hall = NonRectangularRoom(6, 12, "Tile", 0, "Hall")
+flat = Flat("22 Kanatna Street, Odesa", [kitchen, master_bathroom, guest_toilet, living_room, bedroom, children_room, hall])
 
 print(flat)
